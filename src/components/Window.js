20ef7437d481133fab2c4ea966cc6caf83cb571e
Glitch (windows95) <none>
2018-06-20 20:28:43 +0000

    🐾💳 Checkpoint ./src/components/TitleBar.css:291341/869 ./src/components/Window.css:291341/76 ./src/components/Window.js:291341/670 ./src/Windows95.js:291341/124

--- src/components/Window.js
+++ src/components/Window.js
@@ -1,17 +1,39 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import TitleBar from './TitleBar'
 import './Window.css'
 
 class Window extends Component {
   render () {
+    const style = {
+      width: `${this.props.width}px`,
+      height: `${this.props.height}px`,
+      left: `${this.props.x}px`,
+      top: `${this.props.y}px`,
+    }
+
     return (
-      <div className="Window">
+      <div className="Window" style={style}>
         <TitleBar handleClose={() => {}} />
         <div className="MenuBar" />
         <div className="Window-main" />
       </div>
     )
   }
+}
+
+Window.propTypes = {
+  width: PropTypes.number,
+  height: PropTypes.number,
+  x: PropTypes.number,
+  y: PropTypes.number
+}
+
+Window.defaultProps = {
+  width: 400,
+  height: 300,
+  x: 0,
+  y: 0
 }
 
 export default Window
