c1c664204acdaa278b119cac27f0d41ce115e71c
Glitch (windows95) <none>
2018-06-20 21:00:13 +0000

    🏇🎊 Checkpoint ./src/components/NotificationArea.css:291341/142 ./src/components/Window.css:291341/4802 ./src/components/Window.js:291341/18

--- src/components/Window.js
+++ src/components/Window.js
@@ -18,7 +18,7 @@ class Window extends Component {
         <div className="MenuBar" />
 
         <div className="Window-main">
-          <div class="frame">
+          <div class="frame win95-well-border">
             <div class="content">
               <p>Lorem ipsum dolor sit amet, no eum quas fuisset accusamus, his eirmod admodum ut, et mea facete sententiae dissentias. Sea augue affert in. An eam illum vituperata. Reque gloriatur no nec, ex vix audiam deseruisse. Has quas graeco docendi te, his cu option forensibus definitiones. Eos probatus phaedrum no, no eos wisi meliore, est ne viderer legendos.</p>
               <p>Nam percipitur neglegentur ei, vocibus temporibus signiferumque ut mea, pro modo nemore oporteat ne. Vis no alienum prodesset, pri nostrud dolorum definitiones ex. Vix ad audire democritum, te nam sapientem disputationi. Usu id reque suavitate sententiae, eu dolor ullamcorper theophrastus eos, id has vocibus legendos. Dicat antiopam iudicabit cu nec, eam vidit partem volumus ex, diceret albucius consulatu mea no.</p>
