ce0778b80fb98d9a73c7871011d783c3287f8330
Glitch (maze-target) <none>
2018-06-20 17:50:48 +0000

    🎽🍀 Checkpoint ./src/index.css:291341/39 ./.glitch-assets:291341/790 ./src/components/Taskbar.js:291341/907 ./src/components/Taskbar.css:291341/167 ./src/App.js:291341/56

---
+++ src/components/Taskbar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './Taskbar.css'
+
+class Taskbar extends Component {
+  render() {
+    return (
+      <div className="Taskbar">
+        <button className="StartButton" />
+      </div>
+    )
+  }
+}
+
+export default Taskbar
