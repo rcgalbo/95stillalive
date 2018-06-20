142b960eb663e784865c7d8875482b4488f66238
Glitch (windows95) <none>
2018-06-20 22:13:33 +0000

    👬🦃 Checkpoint ./src/components/StartButton.js:291341/200 ./src/components/StartButton.css:291341/401

--- src/components/StartButton.js
+++ src/components/StartButton.js
@@ -5,6 +5,7 @@ class StartButton extends Component {
   render () {
     return (
       <button className="StartButton">
+        <img src="https://cdn.glitch.com/87ebf192-d762-4a8c-b219-92029a8531ce%2Fwin95_logo.png?1529532068066" alt="" />
         Start
       </button>
     )
