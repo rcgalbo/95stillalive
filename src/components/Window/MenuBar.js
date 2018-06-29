e24b0392146fe58eabeb09f8050de39e657c1327
Glitch (windows95) <none>
2018-06-29 13:44:04 +0000

    📯📰 Checkpoint ./src/components/Window/MenuBar.js:291341/1471 ./src/components/Window/MenuBar.css:291341/17

--- src/components/Window/MenuBar.js
+++ src/components/Window/MenuBar.js
@@ -2,13 +2,43 @@ import React, { Component } from 'react'
 import './MenuBar.css'
 
 class MenuBar extends Component {
+  constructor (props) {
+    super(props)
+  
+    this.state = {
+      whichSelected: null
+    }
+    
+    this.renderMenuBarItem = this.renderMenuBarItem.bind(this)      
+  }
+  
+  handleMenuItemClick (event, label) {
+    this.setState({
+      whichSelected: label
+    })
+  }
+    
+  renderMenuBarItem (label, isSelected) {
+    const classNames = ['MenuBarItem']
+    
+    if (isSelected) {
+      classNames.push('MenuBarItem-selected')
+    }
+    
+    return <div key={label} className={classNames.join(' ')} onClick={(event) => this.handleMenuItemClick(event, label)}>{label}</div>
+  }
+  
+  renderMenuBarItems (labels, whichSelected) {
+    return labels.map(label => this.renderMenuBarItem(label, label === whichSelected))
+  }
+
   render () {
+    const labels = ['File', 'Edit', 'View', 'Help']
+    const menuItems = this.renderMenuBarItems(labels, this.state.whichSelected)
+
     return (
       <div className="MenuBar">
-        <div className="MenuBarItem">File</div>
-        <div className="MenuBarItem">Edit</div>
-        <div className="MenuBarItem">View</div>
-        <div className="MenuBarItem">Help</div>
+        {menuItems}
       </div>
     )
   }
